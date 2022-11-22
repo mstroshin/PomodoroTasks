3198806f66c86a05955da441e1d556e01ee09919
Maxim Troshin <mstroshin@gmail.com>
2022-11-22 18:03:43 +0300

    Initial Commit

---
+++ PomodoroTasks/PomodoroTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTasksApp.swift
+//  PomodoroTasks
+//
+//  Created by Maksim Troshin on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
